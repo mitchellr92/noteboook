0edce50b014f46e1e56ea0cb29d32ec760537643
Mitchell Robles <mitchellr92@live.com>
2019-06-27 14:55:35 -0700

    Created basic note and edit note components

---
+++ src/Components/EditNote/EditNote.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './EditNote.css';
+
+class EditNote extends Component {
+    render() {
+        return (
+            <div>Hello World</div>
+        )
+    }
+}
+
+export default EditNote;
---
+++ src/Components/Note/Note.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import "./Note.css";
+
+class Note extends Component {
+  render() {
+    return <div>Hello World</div>;
+  }
+}
+
+export default Note;
